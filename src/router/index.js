d6a72d0e323f347c071ec632452554bf28bf59fc
e <769549170@qq.com>
2021-04-22 16:39:54 +0800

    完成form填写的页面

--- src/router/index.js
+++ src/router/index.js
@@ -122,26 +122,26 @@ export const constantRoutes = [
           },
         ],
       },
-      // {
-      //   path: "menu1",
-      //   component: () => import("@/views/nested/menu1/index"),
-      //   name: "Menu2",
-      //   meta: { title: "数据展示" },
-      //   children: [
-      //     {
-      //       path: "menu1-1",
-      //       component: () => import("@/views/nested/menu1/menu1-1"),
-      //       name: "Menu1-1",
-      //       meta: { title: "建档期1" },
-      //     },
-      //     {
-      //       path: "menu1-3",
-      //       component: () => import("@/views/nested/menu1/menu1-3"),
-      //       name: "Menu1-3",
-      //       meta: { title: "建档前" },
-      //     },
-      //   ],
-      // },
+      {
+        path: "menu1",
+        component: () => import("@/views/nested/menu1/index"),
+        name: "Menu2",
+        meta: { title: "数据展示" },
+        children: [
+          {
+            path: "menu1-1",
+            component: () => import("@/views/nested/menu1/menu1-1"),
+            name: "Menu1-1",
+            meta: { title: "建档期1" },
+          },
+          {
+            path: "menu1-3",
+            component: () => import("@/views/nested/menu1/menu1-3"),
+            name: "Menu1-3",
+            meta: { title: "建档前" },
+          },
+        ],
+      },
     ],
   },
 
--- src/views/nested/menu2/menu1-1/index.vue
+++ src/views/nested/menu2/menu1-1/index.vue
@@ -1,94 +1,40 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item
-        prop="name"
-        label="活动名称"
-        :rules="[{ required: true, message: '活动名称不能为空' }]"
+    <el-tabs v-model="activeName" type="border-card" @tab-click="handleClick">
+      <el-tab-pane label="募投项目基本信息" name="first">
+        <Form @submit="submitForm" ref="form" />
+      </el-tab-pane>
+      <el-tab-pane label="考核指标情况" name="second"
+        >考核指标情况1</el-tab-pane
       >
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker
-            type="date"
-            placeholder="选择日期"
-            v-model="form.date1"
-            style="width: 100%;"
-          ></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker
-            placeholder="选择时间"
-            v-model="form.date2"
-            style="width: 100%;"
-          ></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('form')">提交</el-button>
-        <el-button @click="resetForm('form')">重置</el-button>
-      </el-form-item>
-    </el-form>
+      <el-tab-pane label="考核指标情况" name="third">考核指标情况2</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+import Form from "./_components/Form";
 export default {
+  components: { Form },
   data() {
     return {
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-      },
+      activeName: "first",
     };
   },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
+    handleClick(tab, event) {
+      console.log(tab, event);
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+    submitForm(form) {
+      console.log(form);
+      // this.$refs[formName].validate((valid) => {
+      //   if (valid) {
+      //     console.log(111);
+      //   } else {
+      //     console.log("error submit!!");
+      //     return false;
+      //   }
+      // });
     },
   },
 };
@@ -97,5 +43,14 @@ export default {
 <style scoped>
 .line {
   text-align: center;
+}
+::v-deep .el-tabs--border-card {
+  border-top: none;
+}
+::v-deep .el-tabs--border-card > .el-tabs__header {
+  background-color: #444;
+  box-sizing: border-box;
+  padding: 20px 20px 0;
+  border-radius: 5px;
 }
 </style>
